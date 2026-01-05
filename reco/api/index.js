1f952c65093ec2cf83951942bc4162f4c31fc529
Karma Founder <nakaiwilliams20@gmail.com>
2026-01-05 02:58:19 +0200

    fix: use createRequestListener (correct export name from @react-router/node)

--- reco/api/index.js
+++ reco/api/index.js
@@ -1,10 +1,9 @@
 import pkg from "@react-router/node";
-const { createRequestHandler } = pkg;
+const { createRequestListener } = pkg;
 
-// Import the build - the build is bundled by Vercel
+// Import the build
 const build = await import("../build/server/index.js");
 
-export default createRequestHandler({
-    build,
+export default createRequestListener(() => build, {
     mode: process.env.NODE_ENV,
 });
